c8abc0d137e5091e65676e24e6d2599f52c50cd3
DMalalan <dinuka.malalanayake@gmail.com>
2016-11-07 14:26:38 -0800

    Fix the @Override notations

--- src/main/java/com/sysensor/app/config/WebSocketConfig.java
+++ src/main/java/com/sysensor/app/config/WebSocketConfig.java
@@ -49,7 +49,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 		}
 	}
 
-	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/hello").withSockJS();
 	}
